acd1967315846298e5efce18ebb41a91bd5d52f7
GrzegorzWoloszyn <pgrzegorz.woloszyn@gmail.com>
2020-02-16 12:42:09 +0100

    JUnit, first test, task 6.3; 1. correction

--- Kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ Kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -20,14 +20,22 @@ public class CollectionTestSuite {
 
     @Test
     public void testOddNumbersExterminatorEmptyList() {
+        //Given
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        //When
         System.out.println("Testing empty list of numbers");
-        oddNumbersExterminator.exterminate();
+        ArrayList result = oddNumbersExterminator.exterminate(new ArrayList<>());
+        //Then
+        System.out.println("Empty List: " + result.isEmpty());
     }
     @Test
     public void testOddNumbersExterminatorNormalList() {
+        //Given
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        //When
         System.out.println("Testing the full list of numbers");
-        oddNumbersExterminator.exterminate();
+        ArrayList result2 = oddNumbersExterminator.exterminate(oddNumbersExterminator.exterminate(new ArrayList<Integer>()));
+        //Then
+        System.out.println("Empty List: " + result2.isEmpty());
     }
 }
