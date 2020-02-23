5599c61d3ad01633f3ee105f6ebe49391e3d7332
GrzegorzWoloszyn <pgrzegorz.woloszyn@gmail.com>
2020-02-23 20:19:02 +0100

    all examples were made

---
+++ Kodilla-testing/src/main/java/com/kodilla/testing/library/LibraryDatabase.java
@@ -0,0 +1,17 @@
+package com.kodilla.testing.library;
+
+import java.util.List;
+
+public interface LibraryDatabase {
+
+    //lists books having title beginning with titleFragment
+    List<Book> listBooksWithCondition(String titleFragment);
+    //list books borrowed by libraryUser
+    List<Book> listBooksInHandsOf(LibraryUser libraryUser);
+
+    //try to rent a book from library, returns true if success, fals if book is unavailable
+    boolean rentABook(LibraryUser libraryUser, Book book);
+
+    //return all books borrowed by libraryUser to the library, return number of books returned back
+    int returnBooks(LibraryUser libraryUser);
+}
