7762da4e08b63f7f2b9ce51601d507e83ac252ee
GrzegorzWoloszyn <60856334+GrzegorzWoloszyn@users.noreply.github.com>
2020-02-28 21:11:32 +0100

    kod.6.5: mockujemy forum dyskusyjne (#9)
    kod.6.5: mockujemy forum dyskusyjne

---
+++ Kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticsExecutor.java
@@ -0,0 +1,40 @@
+package com.kodilla.testing.forum.statistics;
+
+public class StatisticsExecutor {
+
+    public StatisticsExecutor(Statistics statistics) {
+    }
+
+    private int userSize;
+    private int posts;
+    private int comments;
+    private double averageCountOfPostsPerUser;
+    private double averageCountOfUsersComments;
+    private double averageCountOfCommentsPerPost;
+
+    public void calculateAdvStatistics(Statistics statistics) {
+        userSize = statistics.usernames().size();
+        posts = statistics.postCount();
+        comments = statistics.commentsCount();
+        if (posts != 0) {
+            averageCountOfCommentsPerPost = (double)comments / posts;
+        }else averageCountOfCommentsPerPost = -1;
+
+        if(userSize != 0) {
+            averageCountOfUsersComments = (double)comments / userSize;
+            averageCountOfPostsPerUser = (double)posts / userSize;
+        }else {
+            averageCountOfUsersComments = -1;
+            averageCountOfPostsPerUser = -1;
+        }
+    }
+
+    public void showStatistics() {
+        System.out.println("Number of forum users: " + userSize);
+        System.out.println("Number of all posts: " + posts);
+        System.out.println("Number of all comments: " + comments);
+        System.out.println("Average number of posts per user: " + averageCountOfPostsPerUser);
+        System.out.println("Average number of comments per user: " + averageCountOfUsersComments);
+        System.out.println("Average number of comments per post: " + averageCountOfCommentsPerPost);
+    }
+}
---
+++ Kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsExecutorTestSuite.java
@@ -0,0 +1,139 @@
+package com.kodilla.testing.forum.statistics;
+
+import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class StatisticsExecutorTestSuite {
+
+    private static int testCounter;
+
+    @BeforeClass
+    public static void beforeAllTests() {
+        System.out.println("This is the beginning of tests.");
+    }
+
+    @AfterClass
+    public static void afterAllTests() {
+        System.out.println("All tests are finished.");
+    }
+
+    @Before
+    public void beforeEveryTest() {
+        testCounter++;
+        System.out.println("Preparing to execute test #" + testCounter);
+    }
+
+    @Test
+    public void testCalculateAdvStatisticsWith0Post() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+
+        when(statisticsMock.postCount()).thenReturn(0);
+        when(statisticsMock.usernames()).thenReturn(Arrays.asList("John", "Poll", "Eva", "David", "Alex"));
+
+        StatisticsExecutor statisticsExecutor = new StatisticsExecutor(statisticsMock);
+        //When
+        statisticsExecutor.calculateAdvStatistics(statisticsMock);
+        //Then
+        Assert.assertEquals(0, statisticsMock.postCount());
+    }
+
+    @Test
+    public void testCalculateAdvStatisticsWith1000Post() {
+        Statistics statisticsMock = mock(Statistics.class);
+
+        when(statisticsMock.postCount()).thenReturn(1000);
+        when(statisticsMock.usernames()).thenReturn(Arrays.asList("John", "Poll", "Eva", "David", "Alex"));
+
+        StatisticsExecutor statisticsExecutor = new StatisticsExecutor(statisticsMock);
+        //When
+        statisticsExecutor.calculateAdvStatistics(statisticsMock);
+        //Then
+        Assert.assertEquals(1000, statisticsMock.postCount());
+    }
+
+    @Test
+    public void testCalculateAdvStatisticsWith0comments() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+
+        when(statisticsMock.commentsCount()).thenReturn(0);
+        when(statisticsMock.usernames()).thenReturn(Arrays.asList("John", "Poll", "Eva", "David", "Alex"));
+
+        StatisticsExecutor statisticsExecutor = new StatisticsExecutor(statisticsMock);
+        //When
+        statisticsExecutor.calculateAdvStatistics(statisticsMock);
+        //Then
+        Assert.assertEquals(0, statisticsMock.commentsCount());
+
+    }
+
+    @Test
+    public void testCalculateAdvStatisticsWithMorePostsThanComments() {
+        Statistics statisticsMock = mock(Statistics.class);
+        when(statisticsMock.commentsCount()).thenReturn(10);
+        when(statisticsMock.postCount()).thenReturn(100);
+        when(statisticsMock.usernames()).thenReturn(Arrays.asList("John", "Poll", "Eva", "David", "Alex"));
+
+        StatisticsExecutor statisticsExecutor = new StatisticsExecutor(statisticsMock);
+        //When
+        statisticsExecutor.calculateAdvStatistics(statisticsMock);
+        //Then
+        Assert.assertTrue(statisticsMock.postCount() > statisticsMock.commentsCount());
+    }
+
+    @Test
+    public void testCalculateAdvStatisticsWithMorsCommentsThanPosts() {
+        Statistics statisticsMock = mock(Statistics.class);
+        when(statisticsMock.commentsCount()).thenReturn(100);
+        when(statisticsMock.postCount()).thenReturn(10);
+        when(statisticsMock.usernames()).thenReturn(Arrays.asList("John", "Poll", "Eva", "David", "Alex"));
+
+        StatisticsExecutor statisticsExecutor = new StatisticsExecutor(statisticsMock);
+        //When
+        statisticsExecutor.calculateAdvStatistics(statisticsMock);
+        //Then
+        Assert.assertTrue(statisticsMock.postCount() < statisticsMock.commentsCount());
+    }
+
+    @Test
+    public void testCalculateAdvStatisticsWith0Users() {
+        Statistics statisticsMock = mock(Statistics.class);
+        when(statisticsMock.commentsCount()).thenReturn(100);
+        when(statisticsMock.postCount()).thenReturn(10);
+        when(statisticsMock.usernames()).thenReturn(Collections.emptyList());
+
+        StatisticsExecutor statisticsExecutor = new StatisticsExecutor(statisticsMock);
+        //When
+        statisticsExecutor.calculateAdvStatistics(statisticsMock);
+        //Then
+        Assert.assertEquals(0, statisticsMock.usernames().size());
+    }
+
+    @Test
+    public void testCalculateAdvStatisticsWith100Users() {
+        Statistics statisticsMock = mock(Statistics.class);
+        when(statisticsMock.commentsCount()).thenReturn(100);
+        when(statisticsMock.postCount()).thenReturn(10);
+        List<String> users = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            users.add(i + ". user");
+        }
+
+        when(statisticsMock.usernames()).thenReturn(users);
+
+        StatisticsExecutor statisticsExecutor = new StatisticsExecutor(statisticsMock);
+        //When
+        statisticsExecutor.calculateAdvStatistics(statisticsMock);
+        //Then
+        Assert.assertEquals(100, statisticsMock.usernames().size());
+    }
+
+}
