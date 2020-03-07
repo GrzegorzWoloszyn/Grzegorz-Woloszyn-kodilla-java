95b956212616739352105c89bdad59a3fa6c1727
GrzegorzWoloszyn <pgrzegorz.woloszyn@gmail.com>
2020-03-07 12:25:29 +0100

    kod7.5.task: obliczamy srednia, funkcyjnie.

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -0,0 +1,20 @@
+package com.kodilla.stream.array;
+
+import sun.jvm.hotspot.ui.tree.SimpleTreeGroupNode;
+
+import java.util.stream.IntStream;
+
+public interface ArrayOperations {
+    static double getAverage(int[] numbers) {
+
+        IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .forEach(n -> System.out.print(n + " "));
+
+        return IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .average()
+                .orElse(0);
+
+    }
+}
