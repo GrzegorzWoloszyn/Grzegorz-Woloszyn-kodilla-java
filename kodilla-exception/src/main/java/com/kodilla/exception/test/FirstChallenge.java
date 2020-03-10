2ad3c0c6da4f17a386a12854ef7331b99b6dd5cc
GrzegorzWoloszyn <pgrzegorz.woloszyn@gmail.com>
2020-03-10 21:29:29 +0100

    kod.8.2.task:try! catch! finally!

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,27 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+    public double divide(double a, double b) throws ArithmeticException {
+
+        if (b == 0) {
+            throw  new ArithmeticException();
+        }
+
+        return a /b;
+    }
+
+    public static void main(String[] args) {
+        double result = 0;
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        try {
+            result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+
+        } catch (ArithmeticException e) {
+            System.out.println("The divider must be larger than zero");
+        } finally {
+            System.out.println("Try again.");
+        }
+    }
+}
