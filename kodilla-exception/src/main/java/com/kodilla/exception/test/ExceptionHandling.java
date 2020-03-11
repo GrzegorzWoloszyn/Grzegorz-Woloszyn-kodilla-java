76e3226ec09b1b67e833d83128ece861a5bce82f
GrzegorzWoloszyn <pgrzegorz.woloszyn@gmail.com>
2020-03-11 08:35:14 +0100

    kod.8.3.task: Obsługa wyjatku

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,14 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        SecondChallenge secondChallenge = new SecondChallenge();
+        try {
+            secondChallenge.probablyIWillThrowException(2,2);
+        } catch (Exception e) {
+            System.out.println("Ups");
+        } finally {
+            System.out.println("Try again");
+        }
+    }
+}
