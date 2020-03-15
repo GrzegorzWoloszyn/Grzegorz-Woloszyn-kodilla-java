8042864052c56d287acb12e40e78980487fe5bd5
GrzegorzWoloszyn <60856334+GrzegorzWoloszyn@users.noreply.github.com>
2020-03-15 12:40:19 +0100

    Kod.8.4.task (#19)
     kod.8.4.example: obsługiwanie błędów na różnych przypadkach

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -0,0 +1,30 @@
+package com.kodilla.exception.test;
+
+import java.util.*;
+
+public class FlightFinder {
+
+    public List<Flight> opensAirs = new ArrayList<>();
+
+        public List<Flight> findFlight (Flight flight) throws RouteNotFoundException {
+
+            Map<String, Boolean> flights = new HashMap<>();
+            flights.put("Warsaw", true);
+            flights.put("Berlin", false);
+            flights.put("Frankfurt", false);
+            flights.put("Gdansk", true);
+            flights.put("London", false);
+            flights.put("Rome", false);
+            flights.put("Paris", true);
+            flights.put("Madrit", false);
+
+            if (flights.containsKey(flight.getArrivalAirport())) {
+                if (flights.get(flight.getArrivalAirport())){
+                   opensAirs.add(flight);
+                }
+            } else {
+                throw new RouteNotFoundException();
+            }
+            return opensAirs;
+        }
+}
