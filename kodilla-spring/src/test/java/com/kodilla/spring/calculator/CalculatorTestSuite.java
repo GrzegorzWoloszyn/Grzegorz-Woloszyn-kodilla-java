2bfae6bd9b5c5b84d38966b3fdb983d30257fbc6
GrzegorzWoloszyn <pgrzegorz.woloszyn@gmail.com>
2020-04-24 20:49:49 +0200

    kod.14.2.task: kalkulator z wyswietlaczem

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,28 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+public class CalculatorTestSuite {
+    @Test
+    public void testCalculations() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.calculator");
+
+        Calculator calculator = (Calculator) context.getBean("calculator");
+
+        double sum = calculator.add(5.5, 10.1);
+        double subRsult = calculator.sub(5.5, 10.1);
+        double product = calculator.mul(5.5, 10.1);
+        double quotient = calculator.div(5.5, 10.1);
+
+        Assert.assertEquals(15.6, sum, 0.001);
+        Assert.assertEquals(-4.6, subRsult, 0.001);
+        Assert.assertEquals(55.55, product, 0.001);
+        Assert.assertEquals(0.544, quotient, 0.01);
+
+    }
+}
