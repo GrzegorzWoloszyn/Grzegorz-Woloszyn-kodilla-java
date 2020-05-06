ea8c36a3209bbe57e9e20933df96e709ecb510da
GrzegorzWoloszyn <pgrzegorz.woloszyn@gmail.com>
2020-05-06 23:07:33 +0200

    kod17.1.task: konfigurowanie srodowiska

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTest.java
@@ -0,0 +1,15 @@
+package com.kodilla.hibernate;
+
+import jdk.internal.jline.internal.TestAccessible;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KodillaHibernateApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
