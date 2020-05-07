5b35e8c314b9d6cfc8309d4e1733f88c1c89d5b1
GrzegorzWoloszyn <pgrzegorz.woloszyn@gmail.com>
2020-05-07 18:31:32 +0200

    code corrected

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTest.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTest.java
@@ -1,7 +1,9 @@
 package com.kodilla.hibernate;
 
-import jdk.internal.jline.internal.TestAccessible;
-import org.springframework.boot.test.context.SpringBootTest;
+        import org.junit.Test;
+        import org.junit.runner.RunWith;
+        import org.springframework.boot.test.context.SpringBootTest;
+        import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
