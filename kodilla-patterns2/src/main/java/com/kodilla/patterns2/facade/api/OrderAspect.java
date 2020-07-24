40e6cad1569cafdc6f1818a5881f4334db5ff60a
GrzegorzWoloszyn <pgrzegorz.woloszyn@gmail.com>
2020-07-24 22:10:44 +0200

    code review

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderAspect.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderAspect.java
@@ -1,7 +1,7 @@
 package com.kodilla.patterns2.facade.api;
 
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 public class OrderAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderAspect.class);
 
-    @After("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
     public void logEvent() {
         LOGGER.info("Method: processOrder, is called " );
     }
