622d0e288ff39f3df139131c23e10e44bba02353
GrzegorzWoloszyn <pgrzegorz.woloszyn@gmail.com>
2020-08-10 22:32:02 +0200

    class EbayTestingApp corrected

--- src/main/java/com/kodilla/testing2/ebay/EbayTestingApp.java
+++ src/main/java/com/kodilla/testing2/ebay/EbayTestingApp.java
@@ -7,14 +7,14 @@ import org.openqa.selenium.WebElement;
 
 public class EbayTestingApp {
 
-    public static final String SEARCHFIELD = "gh-tb ui-autocomplete-input";
+    public static final String SEARCHFIELD = "gh-ac";
 
     public static void main (String []args) {
 
-        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
         driver.get("https://www.ebay.com");
 
-        WebElement searchLaptop = driver.findElement(By.className(SEARCHFIELD));
+        WebElement searchLaptop = driver.findElement(By.id(SEARCHFIELD));
         searchLaptop.sendKeys("Laptop");
         searchLaptop.submit();
     }
